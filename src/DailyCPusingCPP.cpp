63386273d2ab12f39c623ca1338d2ae4d92825dc
SouravVermaCertirfied <62212080+SouravVermaCertirfied@users.noreply.github.com>
2021-08-08 17:42:25 +0530

    Push

---
+++ src/DailyCPusingCPP.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : DailyCPusingCPP.cpp
+// Author      : Sourav Kumar Verma
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "YOY" << endl; // prints YOY
+	return 0;
+}
