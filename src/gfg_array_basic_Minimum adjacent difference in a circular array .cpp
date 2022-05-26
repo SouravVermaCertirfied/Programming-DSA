b9fec29277f902ca01c1f9b160cc84c2b4c39554
Sourav Kumar Verma <62212080+SouravVermaCertirfied@users.noreply.github.com>
2022-05-27 00:07:29 +0530

    Create gfg_array_basic_Minimum adjacent difference in a circular array .cpp

---
+++ src/gfg_array_basic_Minimum adjacent difference in a circular array .cpp
@@ -0,0 +1,49 @@
+	// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+class Solution{
+    public:
+    //Function to find minimum adjacent difference in a circular array.
+    // arr[]: input array
+    // n: size of array
+    int minAdjDiff(int arr[], int n){    
+        // Your code here
+        int min = abs(arr[0] - arr[n-1]) ;
+        for(int i=0;i<n;i++){
+            if(abs(arr[i]-arr[i+1])<min){
+                min = abs(arr[i]-arr[i+1]);
+            }
+        }
+        return min;
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t;
+    cin>>t; //Input testcases
+    
+    while(t--) //While testcases exist
+    {
+        int n;
+        cin>>n; //Input size of array
+        
+        int arr[n]; //Array of size n
+        
+        for(int i = 0; i < n; i++)
+        {
+            cin>>arr[i]; //input elements of array
+        }
+        Solution ob;
+        
+        cout << ob.minAdjDiff(arr, n) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
