19f2bbf23a3827e548037c7209065ce6973e7ed5
Sourav Kumar Verma <62212080+SouravVermaCertirfied@users.noreply.github.com>
2023-02-26 16:55:09 +0530

    fixes #4

--- VSCode/codehelp_problems/string_checkPalindrome.cpp
+++ VSCode/codehelp_problems/string_checkPalindrome.cpp
@@ -1,20 +1,27 @@
 #include<iostream>
+#include<string.h>
 
 using namespace std;
 
 bool isPalindromeTwoPointerApproch(char input[], int size){
-    bool ans;
+    bool ans = true;
     int left = 0;
+    int right = size-1;
 
-
+    while(left<right){
+        if(input[left++]!=input[right--]){
+            ans = false;
+            break;
+        }
+    }
     return ans;
 }
 
 int main(){
-    char input1[7] = "Souravv";
-    char input2[7] = "racecar";
-    int size = 7;
-    
-    isPalindromeTwoPointerApproch(input2, size);
+    char input1[8] = "Souravv";
+    char input2[8] = "aja";
+    int size = strlen(input1);
+    cout<<"Length of the string is : "<<size<<endl;
+    isPalindromeTwoPointerApproch(input1, size) ? cout<<"Palindrome" : cout<<"Not a Palindrome";
     return 0;
 }
